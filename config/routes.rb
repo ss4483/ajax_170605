50d2f3c9fdfd9a99bd3a8b552ccc1ce303faaabd
Kim SungSoo <ss4483@naver.com>
2017-06-05 09:00:17 +0000

    M:N 관계

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+
+ get 'posts/new' => 'posts#new'
+ post 'posts/create' => 'posts#create'
+
+ root 'posts#index'
+ get 'posts/show/:post_id'=> 'posts#show'
+ 
+ 
+  get 'posts/edit/:post_id' => 'posts#edit'
+ post 'posts/update/:post_id' => 'posts#update'
+ 
+ post 'posts/destroy/:post_id' => 'posts#destroy'
+ 
+ #comment
+ post '/comments/create/:post_id' => 'comments#create'
+ post '/posts/show/:post_id/destroy/:comment_id' => 'comments#destroy'
+end
